a8316785c7213ccc334d5aea8e22a3cbc40fa322
mohammad <m.deheghanifard@gmail.com>
2024-03-03 11:12:44 +0330

    add Fold Terminal Operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,29 +3,39 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.fold
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 
 fun main(): Unit = runBlocking {
-
     launch {
-        factorial(30).collect {
-
-        }
+        factorial(30)
         // Builders
-        val flow1 = flowOf<Int>(20,25,60,70,80,85).collect {  }
-        listOf("ali","mohammad","reza","ayda").asFlow().collect {  }
+        val flow1 = flowOf<Int>(20,25,60,70,80,85)
+        listOf("ali","mohammad","reza","ayda").asFlow()
 
         flow {
-         emit("hello")
+            emit("hello")
             delay(1000)
             emit("how are you?")
             delay(1000)
             emit("im fine thank you.")
-        }.collect {
-            println(it)
         }
+
+        // Fold Terminal Operator
+        val flow2 = flow {
+            emit(30)
+            println("Emit 200")
+            emit(200)
+        }
+
+        val result = flow2.fold(10) { acc , value ->
+            println(acc)
+            println(value)
+            acc + value
+        }
+        println(result)
     }
 
 
