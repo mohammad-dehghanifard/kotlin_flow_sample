4c5ad66727b989a5524ebdfec164abaddfb65507
mohammad <m.deheghanifard@gmail.com>
2024-03-03 11:34:17 +0330

    impl lunch in Terminal Operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,33 @@
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.EmptyCoroutineContext
 
 
-fun main(): Unit = runBlocking {
-    launch {
-        factorial(30)
-        // Builders
-        val flow1 = flowOf<Int>(20,25,60,70,80,85)
-        listOf("ali","mohammad","reza","ayda").asFlow()
+fun main(){
 
-        flow {
-            emit("hello")
-            delay(1000)
-            emit("how are you?")
-            delay(1000)
-            emit("im fine thank you.")
-        }
 
-        // Fold Terminal Operator
-        val flow2 = flow {
-            emit(30)
-            println("Emit 200")
-            emit(200)
-        }
 
-        val result = flow2.fold(10) { acc , value ->
-            println(acc)
-            println(value)
-            acc + value
-        }
-        println(result)
+
+    // lunch in
+    val scope = CoroutineScope(EmptyCoroutineContext)
+    val flow3 = flow {
+        emit(10)
+        emit(35)
+        emit(140)
     }
+    flow3
+        .onEach { it -> println(it) }
+        .launchIn(scope)
 
-
-
+    Thread.sleep(2000)
 }
+
+
+
 
 
 
